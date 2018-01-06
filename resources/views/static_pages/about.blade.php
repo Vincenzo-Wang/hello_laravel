a268e97048b94a3ceb7bf963fad562684e864279
wangcong <uwcong@126.com>
2018-01-06 15:01:34 +0800

    finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','ABOUT PAGE')
+@section('content')
+    <h1>关于页</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','HELP PAGE')
+@section('content')
+    <h1>帮助页</h1>
+@stop
